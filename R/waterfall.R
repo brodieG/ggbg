daf8c09e58964de377e9ebe6d28024872e1e2c9a
brodieG <brodieG@users.noreply.github.com>
2018-04-08 14:13:34 -0400

    starting with waterfall

---
+++ R/waterfall.R
@@ -0,0 +1,57 @@
+#' Waterfall Charts
+#'
+#' Waterfall charts are a variation on [ggplot2::geom_col] charts, where the
+#' base of each bar starts where the top of the previous bar ended.  This is
+#' effectively equivalent to a stacked bar chart that has been spread on the x
+#' axis.
+
+geom_waterfall <- function(
+  mapping = NULL, data = NULL,
+  stat = "identity", position = "stack",
+  ...,
+  width = NULL,
+  na.rm = FALSE,
+  show.legend = NA,
+  inherit.aes = TRUE) {
+
+  layer(
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    geom = GeomWaterfall,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      width = width,
+      na.rm = na.rm,
+      ...
+    )
+  )
+}
+
+
+#' @rdname ggplot2-ggproto
+#' @format NULL
+#' @usage NULL
+#' @export
+#' @include geom-rect.r
+
+GeomWaterfall <- ggproto("GeomWaterfall", GeomRect,
+
+  required_aes = c("x", "y"),
+
+  setup_data = function(data, params) {
+    data$width <- data$width %||%
+      params$width %||% (resolution(data$x, FALSE) * 0.9)
+    transform(data,
+      ymin = pmin(y, 0), ymax = pmax(y, 0),
+      xmin = x - width / 2, xmax = x + width / 2, width = NULL
+    )
+  },
+
+  draw_panel = function(self, data, panel_params, coord, width = NULL) {
+    # Hack to ensure that width is detected as a parameter
+    ggproto_parent(GeomRect, self)$draw_panel(data, panel_params, coord)
+  }
+)
