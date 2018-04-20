4a8b272fa2e13be82f2c1186ca7d8ddcecab19bc
brodieG <brodieG@users.noreply.github.com>
2018-04-19 20:16:40 -0400

    add check_req_aes

--- R/waterfall.R
+++ R/waterfall.R
@@ -1,3 +1,19 @@
+## Copyright (C) 2018  Brodie Gaslam
+##
+## This file is part of "ggbg - Assorted Ggplot Extensions"
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## Go to <https://www.r-project.org/Licenses/GPL-2> for a copy of the license.
+
 #' Waterfall Charts
 #'
 #' Waterfall charts are a variation on [ggplot2::geom_col] charts, where the
@@ -6,6 +22,7 @@
 #' axis.
 #'
 #' @inheritParams ggplot2::geom_bar
+#' @export
 
 geom_waterfall <- function(
   mapping = NULL, data = NULL,
@@ -39,7 +56,8 @@ GeomWaterfall <- ggproto("GeomWaterfall", GeomRect,
 
   required_aes = c("x", "y"),
 
-  setup_data = function(data, params) {
+  setup_data = function(self, data, params) {
+    check_req_aes(self, data)
     data$width <- data$width %||%
       params$width %||% (resolution(data$x, FALSE) * 0.9)
     transform(data,
@@ -49,6 +67,7 @@ GeomWaterfall <- ggproto("GeomWaterfall", GeomRect,
   },
 
   draw_panel = function(self, data, panel_params, coord, width = NULL) {
+    check_req_aes(self, data)
     # width=NULL is hack to ensure that width is detected as a parameter as
     # otherwise it gets trimmed by param trimming process in `draw_layer`
 
