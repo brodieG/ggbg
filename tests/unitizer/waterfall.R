7bfe0fa405522abdcd8252473ba09f79fda69d57
brodieG <brodieG@users.noreply.github.com>
2018-04-21 10:53:01 -0400

    test cases

--- tests/unitizer/waterfall.R
+++ tests/unitizer/waterfall.R
@@ -5,9 +5,34 @@ unitizer_sect("basic", {
   dat <- data.frame(x=10:1, y=1:10)
   p0 <- ggplot(dat) + geom_waterfall()
   ggplot_build(p0)
-  p1 <- ggplot(dat, aes(x=x, y=y)) + geom_waterfall()
+  p1 <- ggplot(dat, aes(x=x, y=y)) + geom_waterfall(position="dodge")
   x <- ggplot_build(p1)
-  p2 <- ggplot(dat, aes(x=x, y=y)) + geom_waterfall() +
-    scale_x_log10() + scale_y_log10()
-  x <- ggplot_build(p2)
+
+  dat2 <- dat
+  dat2[5, "x"] <- 5L
+
+  ggplot(dat2, aes(x=x, y=y)) +
+    geom_waterfall(position="dodge", color="white")
+
+  ggplot(dat2, aes(x=x, y=y)) +
+    geom_waterfall(position=position_dodge2(), color="white")
+
+  dat3 <- dat2
+  dat3[5, "y"] <- -2L
+
+  ggplot(dat3, aes(x=x, y=y)) + 
+    geom_waterfall(color="red", position="dodge")
+
+  dat4 <- dat2
+  dat4[6, "y"] <- -2L
+
+  ggplot(dat3, aes(x=x, y=y)) + 
+    geom_waterfall(color="red", position="dodge")
+
+
+  # Negative values
+
+  dat5 <- data.frame(x=1:3, y=c(1, -2, 4))
+  ggplot(dat5, aes(x=x, y=y)) + geom_waterfall(position="identity")
+
 })
