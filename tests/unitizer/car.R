b78be49509afa5bea87d14d5eb6dfd624dcbd532
brodieG <brodieG@users.noreply.github.com>
2018-06-01 21:30:57 -0400

    car tests

--- tests/unitizer/car.R
+++ tests/unitizer/car.R
@@ -5,5 +5,5 @@ unitizer_sect("Car", {
   p <- ggplot(
     geom.car.data, aes(x=x, y=y, length=length, width=width, fill=label)
   ) + geom_car()
-  ggplot_build(p)
-}
+  unclass(ggplot_build(p)$plot)
+})
