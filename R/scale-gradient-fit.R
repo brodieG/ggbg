bdf26ee0bc03108e033d2f31a00ef973a463a51e
brodieG <brodieG@users.noreply.github.com>
2018-08-18 21:02:37 -0400

    initial mock implementation of ScaleContinuousFit

--- R/scale-gradient-fit.R
+++ R/scale-gradient-fit.R
@@ -30,7 +30,35 @@ jet <- t(
     nrow=3
   )
 )
+#' @importFrom ggplot2 ScaleContinuous ggproto
 
-if(FALSE) {
-}
+ScaleContinuousFit <- ggproto("ScaleContinuousFit", ggplot2::ScaleContinuous,
+  map_data = numeric(),
+  map = function() NULL,
+
+  ## We need to collect the totality of the data so that we can do shaped
+  ## mappings, this is why we add the `data` field
+
+  train = function(self, x) {
+    if(!is.numeric(x))
+      stop(
+        "Internal Error: Attempting to train ", class(self),
+        " with non-numeric data."
+      )
+    self$data <- c(self$data, c)
+
+    ## call parent method as well in case other methods used the range data
+    ggproto_parent(ScaleContinuous, self)$train(x)
+  },
+  map = function(self, x, limits = self$get_limits()) {
+    x <- self$rescaler(self$oob(x, range = limits), from = limits)
+
+    uniq <- unique(x)
+    pal <- self$palette(uniq)
+    scaled <- pal[match(x, uniq)]
+
+    ifelse(!is.na(scaled), scaled, self$na.value)
+  },
+  palette = NULL
+)
 
