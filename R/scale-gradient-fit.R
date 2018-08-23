e63dafd545c4c7412080483553ed6cd842529a85
brodieG <brodieG@users.noreply.github.com>
2018-08-22 21:54:57 -0400

    mostly done w/ palette fun generator docs

--- R/scale-gradient-fit.R
+++ R/scale-gradient-fit.R
@@ -67,69 +67,79 @@ ScaleContinuousFit <- ggproto("ScaleContinuousFit", ggplot2::ScaleContinuous,
 #' Create a Palette Function That Produces Uniform Colours
 #'
 #' The resulting palette function converts values between 0 and 1 into colours
-#' along the piecewise path through `colours` in the L*a*b* space.  Pairs of
-#' points that are equidistant in the input domain should be approximately
-#' equidistant along the `colour` path as per `dist.fun`.
+#' along the piecewise path through `colours` in a three dimensional colour
+#' space, L*a*b* by default.  Pairs of points that are equidistant in the input
+#' domain should be approximately equidistant along the `colours` path as per
+#' `dist_fun`, although if the path is not straight they are unlikely to be
+#' equidistant in the colour space itself.
 #'
 #' Distance along the path is measured as the cumulative sum of the
-#' `dist.fun` distance between small segments of the path.
+#' `dist_fun` distance between small segments of the path.  In order to get
+#' the expected outcomes `dist_fun` should return the same distance for any
+#' straight line segment whether it is computed as the distance from one end to
+#' the other, or as the sum of the lengths of sub-segments that constitute
+#' the same path.  Additionally as we move from one point to the other along the
+#' shortest Euclidean path in the three dimensional colour space, the value
+#' `dist_fun` returns should decrease, though it need not do so in proportion to
+#' the Euclidean distance traveled.
 #'
-#' TEST WHETHER WE GET SAME DISTANCE WITH dist.fun IF WE MEASURE SUM OF PIECES.
-#' This seems to be true for deltaE2000.
+#' Upon creation of the function, each segment in `colours` is subdivided into
+#' enough pieces such that the distance spanned by each piece is less than the
+#' tolerance specified in `dist_ctl`.  The value-to-colour map is then carried
+#' out by finding the segment that causes the cumulative accrued `dist_fun`
+#' distance as a fraction of the total cumulative `dist_fun` distance of the
+#' path to exceed the value.  The actual `colour` is then interpolated linearly
+#' in that segment in the provided colour space.
 #'
-#' To the extent the
-#' `colour` path used to build the palette function is not straight, it is
-#' possible for shortest distance between two points to be 
+#' The complexity of this function and the resulting palette function is
+#' designed to accommodate the CIEDE2000 colour distance function, implemented
+#' in this package as [`deltaE2000`].
 #'
-#' that are equal between input points should be approximately equal 
-#'
-#' Uniformity is measured as approximately as per `dist.fun` along the piecewise
-#' path through the `colours` vector in L*a*b* space.
-#'
-#' The resulting `palette` function will convert values between 0 and 1 into the
-#' colour that corresponds to the location along the colour path that is that
-#' fraction of the full length of the colour path as measured by the cumulative
-#' sum of the distances between adjacent colours.
-#' attempt to approximate the colour that
-#' corresponds to 
-#'
-#' A number of colours is interpolated along the shortest Euclidean path in
-#' L*a*b* space that passes through all the colours specified in `colours`, in
-#' the order they are specified.  The number will be such that it is more than
-#' `min.colors` and large enough that the distance measured with `dist.fun`
-#' between any two adjacent colours is less than `min.dist`.  This allows us to
-#' easily compute cumulative distances as per `dist.fun` along the colour path.
-#' If we need to compute a cumulative distance that lies between two of the
-#' computed colours, we interpolate that partial distance using the full
-#' distance as per `dist.fun` between the two nearest known colours multiplied
-#' by the ratio of the partial Euclidean distance to the full Euclidean distance
-#' measured *along* the colour path (i.e., not in 3D space).
-#'
-#' The palette function can then be given a value between `from` and `to` and
-#' the colour that is approximately at the same point along the colour path as
-#' measured by `dist.fun` will be returned.
-#'
+#' @seealso [`ggplot2::scale_fill_gradientn`], [`grDevices::col2rgb`]
 #' @param colours character a vector of colours to base the palette on, must
 #'   have at least two colours, and the colours should be in a format understood
 #'   by [`grDevices::col2rgb`].
 #' @param mid integer(1L) in `seq_along(colours)`, indicates which element in
 #'   `colours` is to be treated as the midpoint colour.
-#' @seealso [`ggplot2::scale_fill_gradientn`], [`grDevices::col2rgb`]
-#' @param dist.fun a function that computes distances between colors in the
-#'   L*a*b* color space.  The function should have two parameters, which will
-#'   be 3 column matrices containing the L, a, and b coordinates or the colours.
+#' @param dist_fun a function that computes distances between colors in a three
+#'   dimensional color space.  The function should have two parameters, which
+#'   will be 3 column matrices containing the coordinates of the colours to
+#'   compute the distances between.  The function should compute the distances
+#'   between matching rows across the two matrices.
+#' @param na.value character(1L) a colour in a format understood by
+#'   [`grDevices::col2rgb`] to use to represent NA values.
+#' @param col_to_space a function that converts colors of the type accepted by
+#'   [`col2rgb`] into a 3 column matrix representing coordinates of the same
+#'   colour in the target colour space.  The default colour space is L*a*b*.
+#' @param space_to_col a function that carries out the reverse of the
+#'   that `col_to_space` transformation.
+#' @param dist_ctl a list containing the control values used in the
+#'   approximation of the `dist_fun` distance along the path.  It must include
+#'   the following named elements:
+#'
+#'   * `max.adj.dist`: numeric(1L) the maximum allowed distance between two
+#'     adjacent computed distance points.  The smaller this number the more
+#'     accurate the approximation will be, but the trade off is a slower palette
+#'     function.
+#'   * `tol`: passed to [`equalize_dists2`] as the value of the `tol` parameter.
+#'   * `iters`: passed to [`equalize_dists2`] as the value of the `iters`
+#'     parameter.
+#'   * Other parameters to pass on to `dist_fun`; if `dist_fun` uses one of the
+#'     previous three parameter names you will need to write a wrapper function
+#'     that uses different names and forwards them to the distance function.
 
-palette_equi_div <- function(colours, mid, na.value, dist.fun=deltaE2000) {
+palette_equi_div <- function(colours, mid, na.value, dist_fun=deltaE2000) {
 }
 palette_equi <- function(
-  colours, na.value, from=0, to=1, dist.ctl
+  colours, na.value, dist_fun=deltaE2000, dist_ctl,
+  col_to_space=color_to_lab, space_to_col=lab_to_color, from=0, to=1
 ) {
   col.lab <- color_to_lab(colours)
   f_lab <- make_coord_funs(col.lab)
 
   # We want some number of distinct colors to do our look
 
-  col.lab.e <- do.call(equalize_dists2, c(list(col.lab, f_lab), dist.ctl))
+  col.lab.e <- do.call(equalize_dists2, c(list(col.lab, f_lab), dist_ctl))
 
 }
 
